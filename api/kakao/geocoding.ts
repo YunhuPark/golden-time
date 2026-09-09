8aed9dc68ff1329379bcff0cca701743b88605e0
YUNHU PARK <byunhu35@gmail.com>
2026-09-09 16:54:06 +0900

    Use framework-neutral API types

--- api/kakao/geocoding.ts
+++ api/kakao/geocoding.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { ApiRequest, ApiResponse } from '../httpTypes';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: ApiRequest, res: ApiResponse) {
   if (req.method !== 'GET') {
     res.setHeader('Allow', 'GET');
     return res.status(405).json({ error: 'Method Not Allowed' });
